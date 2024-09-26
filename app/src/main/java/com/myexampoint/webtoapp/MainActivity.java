2b71a559d7d69974d16a6f29ce20034f7d099f84
AAV <yangmingjie@yahoo.com>
2024-09-25 17:09:32 -0700

    Update MainActivity.java

--- app/src/main/java/com/myexampoint/webtoapp/MainActivity.java
+++ app/src/main/java/com/myexampoint/webtoapp/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     String ShowOrHideWebViewInitialUse = "show";
     private WebView webview ;
     private ProgressBar spinner;
-    String myurl = "https://instagram.com/"; //Change this  to your website hostname
+    String myurl = "https://www.tinasky.com/"; //Change this  to your website hostname
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
